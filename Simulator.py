119c773388f71973bf3c1d18246485ee847009eb
felipetrentin <felipetrentintecnico@hotmail.com>
2022-03-04 23:33:26 -0300

    add comments and real scale

--- Simulator.py
+++ Simulator.py
@@ -1,12 +1,19 @@
 import pygame
 from numpy import *
+# hand = end affector
+# wrist = last actuator
+# elbow = middle actuator
 
+#theta 1 = shoulder
+#theta 2 = elbow
+#theta 3 = wrist
 from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 def main():
     while True:
+        #detect when close event
         for event in pygame.event.get():
             if event.type == QUIT:
                pygame.quit()
@@ -14,21 +21,24 @@ def main():
         screenCntr = (320, 240)
         mX, mY = pygame.mouse.get_pos()
 
-
-        a1= 100
-        a2 = 80
-        a3 = 20
+        #physical sizes
+        a1= 300/2 #30cm
+        a2 = 180/2 #18cm
+        a3 = 150/2 #15cm
 
         # Desired Position of End effector
         px = -(320 - mX)
         py = 240 - mY
 
-        phi = 180
-        phi = deg2rad(phi)
+        #angle of end affector
+        phi = deg2rad(180)
 
         # Equations for Inverse kinematics
+
+        #get wrist position from hand 
         wx = px - a3*cos(phi)
         wy = py - a3*sin(phi)
+
 
         delta = wx**2 + wy**2
         c2 = (delta -a1**2 -a2**2)/(2*a1*a2)
@@ -43,11 +53,11 @@ def main():
         Theta1 = theta_1
         Theta2 = theta_2
         Theta3 = theta_3
-        print("theta1:{:.4f} theta2:{:.4f} theta3:{:.4f} X:{} Y:{}".format(rad2deg(Theta1), rad2deg(Theta2), rad2deg(Theta3), mX, mY))
+        print("theta1:{:.4f} theta2:{:.4f} theta3:{:.4f} X:{} Y:{}".format(rad2deg(Theta1), (180 - rad2deg(Theta2)), rad2deg(Theta3), px, py))
         screen.fill((0,0,0))
-        elbow = rcord(screenCntr, Theta1, 100)
-        wrist = rcord(elbow, Theta1 + Theta2, 80)
-        hand = rcord(wrist, Theta1 + Theta2 + Theta3, 20)
+        elbow = rcord(screenCntr, Theta1, a1)
+        wrist = rcord(elbow, Theta1 + Theta2, a2)
+        hand = rcord(wrist, Theta1 + Theta2 + Theta3, a3)
         pygame.draw.line(screen, (255,0,0), screenCntr, elbow)
         pygame.draw.line(screen, (0,255,0), elbow, wrist)
         pygame.draw.line(screen, (0,0,255), wrist, hand)
